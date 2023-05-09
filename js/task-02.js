aa9561d8efd01f416fa216decf6680f44cb9ccf6
Morelli01 <120120074+Morelli01@users.noreply.github.com>
2023-05-09 17:23:24 +0300

    /

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,22 @@
+
+const ingredientsList = document.querySelector('#ingredients');
+
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+// Створення елементу <li> з класом "item" для кожного інгредієнту в масиві
+const ingredientsItems = ingredients.map((ingredient) => {
+  const li = document.createElement('li');
+  li.textContent = ingredient;
+  li.classList.add('item');
+  return li;
+});
+
+// Вставка всіх елементів <li> одним разом в список ul#ingredients
+ingredientsList.append(...ingredientsItems);
