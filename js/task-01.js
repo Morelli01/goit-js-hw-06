0bc67a55665d3a222d1d996eec71d543984c4567
Morelli01 <120120074+Morelli01@users.noreply.github.com>
2023-05-23 17:26:51 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,14 +1,9 @@
 const categoriesList = document.querySelector('#categories');
 const categoriesItems = categoriesList.querySelectorAll('.item');
-
-// Порахувати і вивести кількість категорій
 console.log(`Number of categories: ${categoriesItems.length}`);
 
-// Для кожного елемента li.item вивести заголовок і кількість елементів
-categoriesItems.forEach((item) => {
+categoriesItems.forEach(item => {
   const categoryName = item.querySelector('h2').textContent;
   const categoryElements = item.querySelectorAll('li').length;
-
-  console.log(`Category: ${categoryName}`);
-  console.log(`Elements: ${categoryElements}`);
+  console.log(`Category: ${categoryName}\nElements: ${categoryElements}`);
 });
